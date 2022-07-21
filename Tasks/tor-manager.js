74dfc5d6130920d83ba8a4ebf0d6a25f67e75f5b
hybridspyda <hybridspyda@gmail.com>
2022-07-21 07:49:37 +0100

    big update for a while 2022-07-21

--- Tasks/tor-manager.js
+++ Tasks/tor-manager.js
@@ -3,23 +3,23 @@
  * so that another script can buy the port breakers. This script
  * dies a natural death once tor is bought. **/
  export async function main(ns) {
-  const interval = 2000;
+    const interval = 2000;
 
-  var keepRunning = ns.args.length > 0 && ns.args[0] == "-c";
-  if (!keepRunning)
-      ns.print(`tor-manager will run once. Run with argument "-c" to run continuously.`)
+    var keepRunning = ns.args.length > 0 && ns.args[0] == "-c";
+    if (!keepRunning)
+        ns.print(`tor-manager will run once. Run with argument "-c" to run continuously.`)
 
-  let hasTor = () => ns.scan("home").includes("darkweb");
-  if (hasTor())
-      return ns.print('Player already has Tor');
-  do {
-      if (hasTor()) {
-          ns.toast(`Purchased the Tor router!`, 'success');
-          break;
-      }
-      ns.purchaseTor();
-      if (keepRunning)
-          await ns.sleep(interval);
-  }
-  while (keepRunning);
+    let hasTor = () => ns.scan("home").includes("darkweb");
+    if (hasTor())
+        return ns.print('Player already has Tor');
+    do {
+        if (hasTor()) {
+            ns.toast(`Purchased the Tor router!`, 'success');
+            break;
+        }
+        ns.purchaseTor();
+        if (keepRunning)
+            await ns.sleep(interval);
+    }
+    while (keepRunning);
 }
